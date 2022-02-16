0bb62a469227abdc0ee0a140a3dc54c1e029df54
Michał Stoczkowski <stoczkowski.michal@gmail.com>
2022-02-16 16:42:04 +0100

    Introduced base workspace structure and setup

--- apps/consulting/jest.config.js
+++ apps/consulting/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'labs',
+  displayName: 'consulting',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/labs',
+  coverageDirectory: '../../coverage/apps/consulting',
 }
--- apps/consulting/pages/_app.tsx
+++ apps/consulting/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to labs!</title>
+        <title>Welcome to consulting!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
