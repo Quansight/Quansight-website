31ec281cef168a0c6ea2a25817837ab5efa6da7a
kherma <kamil.herma01@gmail.com>
2022-03-06 23:10:50 +0100

    feat: add ui-components lib

--- apps/labs/pages/index.tsx
+++ apps/labs/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Layout } from '@quansight/shared/ui-components';
+
 export function Index(): JSX.Element {
   /*
    * Replace the elements below with your own.
---
+++ libs/shared/ui-components/src/components/layout.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+export const Layout: FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Layout!</h1>
+    </div>
+  );
+};
+
+export default Layout;
