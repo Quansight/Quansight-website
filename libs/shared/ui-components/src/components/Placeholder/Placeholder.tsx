024752b1146efa63be8619894ef9c51a147acbb9
kherma <kamil.herma01@gmail.com>
2022-03-11 05:35:03 +0100

    feat: move Placeholder component to shared ui lib

---
+++ libs/shared/ui-components/src/components/Placeholder/Placeholder.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+type TPlaceholderProps = {
+  componentName: string;
+};
+
+export const Placeholder: FC<TPlaceholderProps> = ({ componentName }) => (
+  <div>
+    The component
+    {componentName}
+    has not been created yet.
+  </div>
+);
+
+export default Placeholder;
