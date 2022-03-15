2cc7f30b1faa277a9fa21838affe8f6d4773a5b3
kherma <kamil.herma01@gmail.com>
2022-03-15 12:03:27 +0100

    feat: update _document to functional component

--- apps/consulting/pages/_document.tsx
+++ apps/consulting/pages/_document.tsx
@@ -1,25 +1,21 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import React from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
+import { FC } from 'react';
 
-type Props = {};
-
-class Document extends NextDocument<Props> {
-  render(): JSX.Element {
-    return (
-      <Html lang="en" className="text-[62.5%]">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Mukta:wght@800&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+const Document: FC = () => {
+  return (
+    <Html lang="en" className="text-[62.5%]">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mukta:wght@800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
 export default Document;
