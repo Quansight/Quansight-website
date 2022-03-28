f66e859e66cc376b09fefaf1815c133f7b72143a
kherma <kamil.herma01@gmail.com>
2022-03-28 13:07:43 +0200

    feat: add job openings desktop styles

--- apps/consulting/components/JobOpenings/JobOpenings.tsx
+++ apps/consulting/components/JobOpenings/JobOpenings.tsx
@@ -3,12 +3,12 @@ import { TJobOpeningsProps } from './types';
 import Link from 'next/link';
 
 const JobOpenings: FC<TJobOpeningsProps> = ({ title, jobs }) => (
-  <section className="flex flex-col justify-center items-center my-24 sm:px-60 lg:px-80">
+  <section className="flex flex-col justify-center items-center my-24 mx-auto xl:mt-52 xl:mb-24 max-w-layout">
     <h2 className="mb-[4.3rem] text-[4rem] font-extrabold leading-[4.9rem] sm:mb-32 sm:text-[4.8rem] text-violet font-heading">
       {title}
     </h2>
     {jobs.length > 0 ? (
-      <ul className="flex flex-col gap-[4.3rem] text-center sm:w-full">
+      <ul className="flex flex-col gap-[4.3rem] text-center sm:w-full sm:max-w-[50rem] lg:max-w-[60rem] xl:max-w-[70rem]">
         {jobs.map(({ _uid, jobTitle, linkUrl, location, isRemote }) => (
           <li key={_uid} className="sm:flex sm:justify-between sm:items-center">
             <Link href={linkUrl}>
@@ -24,7 +24,9 @@ const JobOpenings: FC<TJobOpeningsProps> = ({ title, jobs }) => (
         ))}
       </ul>
     ) : (
-      <p>No open positions</p>
+      <p className="text-[2rem] font-normal leading-[3rem] text-black sm:text-[3rem]">
+        No open positions
+      </p>
     )}
   </section>
 );
