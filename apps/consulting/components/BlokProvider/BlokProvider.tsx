8d920a85e3382641e0333450c4b8daf371c63b23
galdranorn <skiberowska.justyna@wp.pl>
2022-03-29 16:31:13 +0200

    fix: empty line after ts comment

--- apps/consulting/components/BlokProvider/BlokProvider.tsx
+++ apps/consulting/components/BlokProvider/BlokProvider.tsx
@@ -15,6 +15,8 @@ export const BlokProvider: FC<TBlokProviderProps> = ({ blok }) => {
   if (componentsMap[blok.component]) {
     const Component = componentsMap[blok.component];
     const componentProps = getPropsByType(blok);
+
+    // @ts-expect-error TODO
     return <Component {...componentProps} />;
   }
   return <Placeholder componentName={blok ? blok.component : null} />;
