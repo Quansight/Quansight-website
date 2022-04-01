673ca0d8e01cbd65a76b654e31e41bf90c3524fd
galdranorn <skiberowska.justyna@wp.pl>
2022-04-01 12:17:14 +0200

    change: take hero image from sb

--- apps/consulting/components/BlokProvider/mappers/getHeroProps.ts
+++ apps/consulting/components/BlokProvider/mappers/getHeroProps.ts
@@ -2,6 +2,8 @@ import { THeroProps } from '@quansight/shared/ui-components';
 import { THeroRawData } from '../../../types/storyblok/bloks/hero';
 
 export const getHeroProps = (blok: THeroRawData): THeroProps => ({
+  imageSrc: blok.image.filename,
+  imageAlt: blok.image.alt,
   title: blok.title,
   subTitle: blok.subTitle,
   variant: blok.variant,
--- libs/shared/ui-components/src/components/Hero/Hero.tsx
+++ libs/shared/ui-components/src/components/Hero/Hero.tsx
@@ -1,64 +1,62 @@
 import { FC } from 'react';
 import Image from 'next/image';
 import clsx from 'clsx';
-import { HeroVariant } from './types';
-import { getHeroBackground } from './utils/getHeroBackground';
 
-export type THeroProps = {
-  variant: HeroVariant;
-  title: string;
-  subTitle?: string;
-};
+import { THeroProps, HeroVariant } from './types';
 
-export const Hero: FC<THeroProps> = ({ title, subTitle, variant }) => {
+export const Hero: FC<THeroProps> = ({
+  title,
+  subTitle,
+  variant,
+  imageSrc,
+  imageAlt,
+}) => {
   const isLargeHero = variant === HeroVariant.Large;
   const isMediumHero = variant === HeroVariant.Medium;
 
   return (
     <div
-      className={clsx('relative', 'h-[730px]', {
-        'md:h-[970px]': isLargeHero,
-        'md:h-[730px]': isMediumHero,
-      })}
+      className={clsx(
+        'overflow-hidden w-full h-[730px] bg-[#000000]',
+        isLargeHero && 'md:h-[970px]',
+        isMediumHero && 'md:h-[730px]',
+      )}
     >
-      <Image
-        src={getHeroBackground(variant)}
-        layout="fill"
-        objectFit="cover"
-        objectPosition="center"
-      />
-      <div
-        className={clsx(
-          'relative px-[2rem] placeholder:md:px-0 pt-[8.8rem] md:absolute md:pt-0',
-          ...(isLargeHero ? ['md:w-1/2 md:top-52 md:left-[14%]'] : ''),
-          ...(isMediumHero
-            ? ['h-full w-full flex items-center justify-center flex-col']
-            : ''),
-        )}
-      >
-        <h2
+      <div className="relative mx-auto h-full max-w-layout">
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          layout="fill"
+          objectFit="contain"
+          objectPosition="center"
+        />
+        <div
           className={clsx(
-            'text-[5rem] leading-[6rem] text-white font-heading',
-            {
-              'mb-12': isLargeHero,
-              'text-center': !isLargeHero,
-            },
+            'relative px-[2rem] placeholder:md:px-0 pt-[8.8rem] md:absolute md:pt-0',
+            isLargeHero && 'md:top-52 md:left-[14%] md:w-1/2',
+            isMediumHero &&
+              'flex flex-col justify-center items-center w-full h-full',
           )}
         >
-          {title}
-        </h2>
-        {subTitle && (
-          <h3
+          <h2
             className={clsx(
-              'text-[4rem] leading-[4.8rem] text-white font-heading',
-              {
-                'text-center': !isLargeHero,
-              },
+              'text-[5rem] leading-[6rem] text-white font-heading',
+              isLargeHero ? 'mb-12' : 'text-center',
             )}
           >
-            {subTitle}
-          </h3>
-        )}
+            {title}
+          </h2>
+          {subTitle && (
+            <h3
+              className={clsx(
+                'text-[4rem] leading-[4.8rem] text-white font-heading',
+                isMediumHero && 'text-center',
+              )}
+            >
+              {subTitle}
+            </h3>
+          )}
+        </div>
       </div>
     </div>
   );
