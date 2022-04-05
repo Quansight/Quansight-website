c76398f005d9842819f61b4b020236f1b9dff972
kherma <kamil.herma01@gmail.com>
2022-04-05 10:39:54 +0200

    feat: add IntertwinedArticle styles

--- apps/consulting/components/IntertwinedArticle/IntertwinedArticleItem.tsx
+++ apps/consulting/components/IntertwinedArticle/IntertwinedArticleItem.tsx
@@ -8,9 +8,19 @@ export const IntertwinedArticleItem: FC<TIntertwinedArticleItemProps> = ({
   imageAlt,
 }) => {
   return (
-    <section>
-      <div dangerouslySetInnerHTML={createMarkup(text)} />
-      <Picture imageSrc={imageSrc} imageAlt={imageAlt} layout="fill" />
+    <section className="flex flex-col-reverse gap-16 justify-center items-center mb-16 sm:flex-row sm:odd:flex-row-reverse sm:gap-32">
+      <div
+        className="flex flex-col gap-8 text-[1.6rem] font-normal leading-[2.7rem] text-black sm:w-1/2"
+        dangerouslySetInnerHTML={createMarkup(text)}
+      />
+      <div className="relative w-full h-32 bg-red-500 sm:w-1/2 sm:h-[40rem]">
+        <Picture
+          imageSrc={imageSrc}
+          imageAlt={imageAlt}
+          layout="fill"
+          priority
+        />
+      </div>
     </section>
   );
 };
