ca5af0ce47374115409031e291b6978b258241fb
galdranorn <skiberowska.justyna@wp.pl>
2022-04-13 11:17:50 +0200

    fix: remove centering from layout component

--- libs/shared/ui-components/src/components/Layout/Layout.tsx
+++ libs/shared/ui-components/src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 export const Layout: FC = ({ children }) => {
   return (
-    <div className="overflow-hidden text-5xl text-center">
+    <div className="overflow-hidden">
       <main>{children}</main>
     </div>
   );
