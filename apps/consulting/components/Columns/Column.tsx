ce71e5697c06dd7106152873c7ddfae3006873cf
galdranorn <skiberowska.justyna@wp.pl>
2022-04-13 11:16:17 +0200

    feat: columns component

--- apps/consulting/components/Columns/Column.tsx
+++ apps/consulting/components/Columns/Column.tsx
@@ -5,17 +5,14 @@ import { Picture } from '@quansight/shared/ui-components';
 import { TColumnProps } from './types';
 
 const Column: FC<TColumnProps> = ({ title, text, imageSrc, imageAlt }) => (
-  <div>
-    {/* <div>
+  <div className="pb-[3.5rem] lg:w-1/3 border-box">
+    <div className="text-center">
       <Picture imageSrc={imageSrc} imageAlt={imageAlt} width={90} height={90} />
     </div>
-    <h3>
+    <h3 className="my-[1.8rem] text-[2.7rem] font-extrabold leading-[3.5rem] text-black lg:mt-[2.6rem]">
       {title}
     </h3>
-    <p>
-      {text}
-    </p> */}
-    Hello
+    <p className="text-[1.6rem] leading-[2.7rem] text-black">{text}</p>
   </div>
 );
 
--- apps/consulting/components/Columns/Columns.tsx
+++ apps/consulting/components/Columns/Columns.tsx
@@ -5,10 +5,16 @@ import Column from './Column';
 import { TColumnsProps } from './types';
 
 export const Columns: FC<TColumnsProps> = ({ columns }) => (
-  <section>
-    {/* {columns.map((props) => (
+  <section
+    className="
+      px-[3.5rem] my-[2.4rem] mx-auto
+      md:flex md:gap-[2rem] 
+      lg:gap-[7.5rem] lg:px-[13rem]
+      max-w-layout
+    "
+  >
+    {columns.map((props) => (
       <Column {...props} key={props._uid} />
-    ))} */}
-    Hello
+    ))}
   </section>
 );
