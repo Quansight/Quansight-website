62e496cffd11cc1adb14c823f901a281cfd45f09
galdranorn <skiberowska.justyna@wp.pl>
2022-04-21 13:07:29 +0200

    feat: style textArticle component

--- apps/consulting/components/TextArticle/TextArticle.tsx
+++ apps/consulting/components/TextArticle/TextArticle.tsx
@@ -4,9 +4,25 @@ import { TTextArticleProps } from './types';
 
 export const TextArticle: FC<TTextArticleProps> = ({ header, text }) => {
   return (
-    <article className="gap-[7rem] md:flex lg:px-[13rem] max-w-layout">
-      <h2>{header}</h2>
-      <p>{text}</p>
+    <article
+      className="
+        px-[3.5rem] pt-[5.5rem] pb-[2rem] mx-auto
+        md:grid md:grid-cols-[1fr,1fr] md:py-[8rem]
+        lg:grid-cols-[6fr,7fr] lg:px-[13rem]
+        max-w-layout
+      "
+    >
+      <h2
+        className="
+          mb-[2.5rem] text-[4rem] font-extrabold leading-[4.9rem] md:text-[4.8rem] md:leading-[5.5rem]
+           text-violet font-heading
+        "
+      >
+        {header}
+      </h2>
+      <p className="text-[1.6rem] leading-[2.7rem] md:pl-[4rem] md:mt-[1rem]">
+        {text}
+      </p>
     </article>
   );
 };
