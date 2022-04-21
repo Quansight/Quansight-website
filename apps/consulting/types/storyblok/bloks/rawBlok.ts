02e3da4c67e7358126dee45452e1945ce7f8496c
galdranorn <skiberowska.justyna@wp.pl>
2022-04-21 10:20:46 +0200

    feat: related services

--- apps/consulting/types/storyblok/bloks/rawBlok.ts
+++ apps/consulting/types/storyblok/bloks/rawBlok.ts
@@ -6,6 +6,7 @@ import { TFeaturesRawData } from './features';
 import { THeroRawData } from './hero';
 import { TIntertwinedArticleRawData } from './intertwinedArticle';
 import { TJobListRawData } from './jobList';
+import { TRelatedRawData } from './related';
 import { TStickyNotesRawData } from './stickyNotes';
 import { TTeaserRawData } from './teaser';
 import { TTestimonialRawData } from './testimonial';
@@ -19,6 +20,7 @@ export type TRawBlok =
   | THeroRawData
   | TIntertwinedArticleRawData
   | TJobListRawData
+  | TRelatedRawData
   | TStickyNotesRawData
   | TTeaserRawData
   | TTestimonialRawData;
