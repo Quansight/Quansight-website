5b3cfc6f25e39ccd2753e44934030089ec675893
kherma <kamil.herma01@gmail.com>
2022-04-22 16:24:36 +0200

    feat: add Library Intro styles

--- apps/consulting/components/LibraryIntro/LibraryIntro.tsx
+++ apps/consulting/components/LibraryIntro/LibraryIntro.tsx
@@ -5,9 +5,13 @@ export const LibraryIntro: FC<TLibraryIntroProps> = ({
   title,
   description,
 }) => (
-  <section>
-    <h2>{title}</h2>
-    <p>{description}</p>
+  <section className="py-[7.4rem] px-8 mx-auto max-w-layout">
+    <h2 className="mb-[1.9rem] text-[4rem] font-extrabold leading-[4.9rem] text-center sm:mb-[2.5rem] sm:text-[4.8rem] font-heading text-violet">
+      {title}
+    </h2>
+    <p className="mx-auto max-w-[57rem] text-[2rem] font-normal leading-[3rem] text-center sm:text-[2.2rem]">
+      {description}
+    </p>
   </section>
 );
 
