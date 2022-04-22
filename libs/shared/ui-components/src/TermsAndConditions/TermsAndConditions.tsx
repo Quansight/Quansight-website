292356318c3f736497f0b0f25aac2014e3d76cc0
kherma <kamil.herma01@gmail.com>
2022-04-22 15:31:26 +0200

    feat: add Terms & Conditions styles

--- libs/shared/ui-components/src/TermsAndConditions/TermsAndConditions.tsx
+++ libs/shared/ui-components/src/TermsAndConditions/TermsAndConditions.tsx
@@ -6,11 +6,15 @@ export const TermsAndConditions: FC<TTermsAndConditionsProps> = ({
   title,
   paragraphs,
 }) => (
-  <article>
-    <h2>{title}</h2>
-    {paragraphs.map((paragraph) => (
-      <TermsAndConditionsParagraph {...paragraph} key={paragraph._uid} />
-    ))}
+  <article className="py-[5.4rem] px-[1.8rem] mx-auto sm:py-[6.4rem] sm:px-[4.4rem] xl:px-[21.2rem] xl:pt-[11.2rem] xl:pb-[8.6rem] max-w-layout">
+    <div className="pb-[7.6rem] border-b sm:pb-[8.6rem] xl:pb-[12.6rem] border-gray">
+      <h2 className="mb-[8.6rem] text-[4rem] font-extrabold leading-[4.9rem] text-center sm:mx-auto sm:max-w-[70rem] sm:text-[4.8rem] sm:leading-[5.3rem] xl:mb-[12.4rem] font-heading text-violet">
+        {title}
+      </h2>
+      {paragraphs.map((paragraph) => (
+        <TermsAndConditionsParagraph {...paragraph} key={paragraph._uid} />
+      ))}
+    </div>
   </article>
 );
 
--- libs/shared/ui-components/src/TermsAndConditions/TermsAndConditionsParagraph.tsx
+++ libs/shared/ui-components/src/TermsAndConditions/TermsAndConditionsParagraph.tsx
@@ -4,14 +4,18 @@ import { createMarkup } from '@quansight/shared/utils';
 
 export const TermsAndConditionsParagraph: FC<
   TTermsAndConditionsParagraphProps
-> = ({ title, text }) => (
-  <section>
-    <h3>{title}</h3>
-    <div
-      className="order-3 text-[1.4rem] leading-[3.3rem] sm:order-2"
-      dangerouslySetInnerHTML={createMarkup(text)}
-    />
-  </section>
-);
+> = ({ title, text }) => {
+  return (
+    <section className="px-[1.8rem] mb-[4.1rem] last:mb-0 xl:px-[4.4rem]">
+      <h3 className="mb-[1.8rem] text-[2.7rem] font-extrabold leading-[3.5rem] text-black sm:mb-[2.2rem] sm:text-[3.2rem] xl:mb-[2.5rem] xl:leading-[5.3rem] font-heading">
+        {title}
+      </h3>
+      <div
+        className="min-w-full text-[1.6rem] text-black marker:text-black prose sm:text-[1.8rem] xl:leading-[2.7rem]"
+        dangerouslySetInnerHTML={createMarkup(text)}
+      />
+    </section>
+  );
+};
 
 export default TermsAndConditionsParagraph;
