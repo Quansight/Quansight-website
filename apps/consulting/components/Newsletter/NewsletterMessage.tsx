0ae6a21adca4b784d37be4932c4ebd794d7a75b4
galdranorn <skiberowska.justyna@wp.pl>
2022-05-23 15:04:02 +0200

    fix: types import

--- apps/consulting/components/Newsletter/NewsletterMessage.tsx
+++ apps/consulting/components/Newsletter/NewsletterMessage.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 
+import { TNewsletterMessageProps } from './types';
+
 export const NewsletterMessage: FC<TNewsletterMessageProps> = ({ message }) => (
   <p>{message}</p>
 );
