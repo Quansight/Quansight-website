869fd8300c6b39713d244fec52682cd23d899c5c
kherma <kamil.herma01@gmail.com>
2022-05-31 11:00:45 +0200

    feat: clean up

--- libs/shared/ui-components/src/Layout/Layout.tsx
+++ libs/shared/ui-components/src/Layout/Layout.tsx
@@ -4,11 +4,8 @@ import { TLayoutProps } from './types';
 
 export const Layout: FC<TLayoutProps> = ({ footer, header, children }) => {
   return (
-    <div className="overflow-hidden">
+    <div>
       {header}
-      <div className="flex justify-center items-center w-screen h-screen text-white bg-black">
-        <button>Hello</button>
-      </div>
       <main id="maincontent">{children}</main>
       {footer}
     </div>
