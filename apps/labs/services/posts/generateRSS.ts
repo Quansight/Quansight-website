2146e0475c09984bd29801be1bea7ed63a283636
galdranorn <skiberowska.justyna@wp.pl>
2022-06-02 14:18:38 +0200

    refactor: move urls to variables

--- apps/labs/services/posts/generateRSS.ts
+++ apps/labs/services/posts/generateRSS.ts
@@ -3,33 +3,29 @@ import * as path from 'path';
 
 import { Feed } from 'feed';
 
+import { RSS_DIRECTORY_PATH } from '../../services/api/posts/constants';
 import { TPost } from '../../types/storyblok/bloks/posts';
 
-const URLS = {
-  site: 'https://labs.quansight.org',
-  github: 'https://github.com',
-  rss: 'apps/labs/public/rss.xml',
-};
-
 export const generateRSS = async (posts: TPost[]): Promise<void> => {
-  const rssPath = path.join(process.cwd(), URLS.rss);
+  const siteUrl = process.env.DOMAIN;
+  const rssPath = path.join(process.cwd(), RSS_DIRECTORY_PATH);
   const currentDate = new Date();
 
   const rssFeed = new Feed({
     title: 'Quansight Labs',
     description:
       'This is an RSS feed. To subscribe to it, copy its address and paste it when your feed reader asks for it. It will be updated periodically in your reader.',
-    id: URLS.site,
-    link: URLS.site,
-    image: `${URLS.site}/logo.jpeg`,
-    favicon: `${URLS.site}/favicon.ico`,
+    id: siteUrl,
+    link: siteUrl,
+    image: `${siteUrl}/logo.jpeg`,
+    favicon: `${siteUrl}/favicon.ico`,
     copyright: `All rights reserved ${currentDate.getFullYear()}, Quansight`,
     author: {
       name: 'Quansight Labs',
-      link: URLS.site,
+      link: siteUrl,
     },
     feedLinks: {
-      rss2: `${URLS.site}/rss.xml`,
+      rss2: `${siteUrl}/rss.xml`,
     },
   });
 
@@ -38,12 +34,12 @@ export const generateRSS = async (posts: TPost[]): Promise<void> => {
       title: post.meta.title,
       description: post.meta.description,
       date: new Date(post.meta.published),
-      image: `${URLS.site}${post.meta.featuredImage.src}`,
-      link: `${URLS.site}/blog/${post.slug}`,
+      image: `${siteUrl}${post.meta.featuredImage.src}`,
+      link: `${siteUrl}/blog/${post.slug}`,
       author: [
         {
           name: post.meta.author.fullName,
-          link: `${URLS.github}/${post.meta.author.nickName}`,
+          link: `https://github.com/${post.meta.author.nickName}`,
         },
       ],
     });
