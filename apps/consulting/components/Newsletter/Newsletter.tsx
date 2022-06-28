54ec95dd68a227eb3399c8e83b67f89d1c840bb2
Marcin Antczak <marcin.antczak@naturaily.com>
2022-06-28 06:47:21 +0200

    fix: change rendering of columns

--- apps/consulting/components/Newsletter/Newsletter.tsx
+++ apps/consulting/components/Newsletter/Newsletter.tsx
@@ -45,7 +45,7 @@ export const Newsletter: FC = () => {
         'md:py-[4rem] md:px-[10rem] md:my-[5rem]',
       )}
     >
-      <NewsletterHeader text="Subscribe for Updates & News" />
+      <NewsletterHeader text="Stay informed with our newsletter" />
       <form className="md:flex md:gap-[2.6rem]" onSubmit={subscribeNewsletter}>
         <div className="relative md:grow">
           <input
