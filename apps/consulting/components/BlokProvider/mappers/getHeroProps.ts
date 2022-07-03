4a88667ce82020d73983691d0a73b0461365c167
kherma <kamil.herma01@gmail.com>
2022-07-03 23:45:28 +0200

    feat: add hero images labs support

--- apps/consulting/components/BlokProvider/mappers/getHeroProps.ts
+++ apps/consulting/components/BlokProvider/mappers/getHeroProps.ts
@@ -2,31 +2,27 @@ import { THeroProps } from '@quansight/shared/ui-components';
 
 import { THeroRawData } from '../../../types/storyblok/bloks/hero';
 
-export const getHeroProps = (blok: THeroRawData): THeroProps => {
-  console.log(blok);
-
-  return {
-    imageSrc: blok.image.filename,
-    imageAlt: blok.image.alt,
-    title: blok.title,
-    subTitle: blok.subTitle,
-    variant: blok.variant,
-    objectFit: blok.objectFit,
-    backgroundColor: blok?.backgroundColor,
-    imageMobile: {
-      imageSrc: blok?.imageMobile?.filename,
-      imageAlt: blok?.imageMobile?.alt,
-      objectFit: blok?.objectFitMobile,
-    },
-    imageTablet: {
-      imageSrc: blok?.imageTablet?.filename,
-      imageAlt: blok?.imageTablet?.alt,
-      objectFit: blok?.objectFitTablet,
-    },
-    imageDesktop: {
-      imageSrc: blok?.imageDesktop?.filename,
-      imageAlt: blok?.imageDesktop?.alt,
-      objectFit: blok?.objectFitDesktop,
-    },
-  };
-};
+export const getHeroProps = (blok: THeroRawData): THeroProps => ({
+  imageSrc: blok.image.filename,
+  imageAlt: blok.image.alt,
+  title: blok.title,
+  subTitle: blok.subTitle,
+  variant: blok.variant,
+  objectFit: blok.objectFit,
+  backgroundColor: blok?.backgroundColor,
+  imageMobile: {
+    imageSrc: blok?.imageMobile?.filename,
+    imageAlt: blok?.imageMobile?.alt,
+    objectFit: blok?.objectFitMobile,
+  },
+  imageTablet: {
+    imageSrc: blok?.imageTablet?.filename,
+    imageAlt: blok?.imageTablet?.alt,
+    objectFit: blok?.objectFitTablet,
+  },
+  imageDesktop: {
+    imageSrc: blok?.imageDesktop?.filename,
+    imageAlt: blok?.imageDesktop?.alt,
+    objectFit: blok?.objectFitDesktop,
+  },
+});
