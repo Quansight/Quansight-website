de5b5c70884f1d2973e47c21fb4dcab910bbb101
kherma <kamil.herma01@gmail.com>
2022-07-04 02:51:10 +0200

    feat: add condition to members list margin

--- libs/shared/ui-components/src/Team/Team.tsx
+++ libs/shared/ui-components/src/Team/Team.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
 
+import clsx from 'clsx';
+
 import { TeamMember } from './TeamMember/TeamMember';
-import { TeamVariant, TTeamProps } from './types';
+import { TeamVariant, TTeamProps, TeamDisplay } from './types';
 import { filterTeam, getRandomMembers } from './utils';
 
 export const Team: FC<TTeamProps> = ({
@@ -17,17 +19,34 @@ export const Team: FC<TTeamProps> = ({
       ? getRandomMembers(filteredTeam, 3)
       : filteredTeam;
 
+  const needHeadingSpace = teamToDisplay?.reduce(
+    (prevValue, { displayName, firstName, lastName }) => {
+      const rawName =
+        displayName === TeamDisplay.Full
+          ? `${firstName} ${lastName}`
+          : firstName;
+
+      return rawName?.length <= 15 && prevValue === false ? false : true;
+    },
+    false,
+  );
+
   return (
     <section className="px-[1.8rem] my-[6rem] mx-auto md:my-[8rem] lg:px-[3rem] xl:px-[18rem] max-w-layout">
       <h2
         className="
-          text-[4rem] font-extrabold leading-[4.9rem] text-center md:text-[4.8rem] 
+          text-[4rem] font-extrabold leading-[4.9rem] text-center md:text-[4.8rem]
           text-violet font-heading
         "
       >
         {header}
       </h2>
-      <ul className="flex flex-wrap mt-[1.5rem] md:justify-center">
+      <ul
+        className={clsx(
+          'flex flex-wrap md:justify-center',
+          needHeadingSpace ? 'mt-[4rem]' : 'mt-[1.5rem]',
+        )}
+      >
         {teamToDisplay
           ?.filter((item) => item)
           .map((item) => (
