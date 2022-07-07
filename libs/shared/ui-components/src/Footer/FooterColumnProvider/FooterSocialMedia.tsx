b1328fed8fcbdf2070f8d3af0a26f631fcb3c380
kherma <kamil.herma01@gmail.com>
2022-07-07 17:59:27 +0200

    fix: change icons container to wrap

--- libs/shared/ui-components/src/Footer/FooterColumnProvider/FooterSocialMedia.tsx
+++ libs/shared/ui-components/src/Footer/FooterColumnProvider/FooterSocialMedia.tsx
@@ -11,7 +11,7 @@ export const FooterSocialMedia: FC<TFooterSocialMediaProps> = ({
     <h2 className="hidden pb-[1.3rem] mb-8 text-[1.6rem] font-bold leading-[3rem] border-b-[0.5px] border-black sm:block xl:border-white">
       {title}
     </h2>
-    <ul className="flex gap-12 lg:gap-8 xl:gap-12">
+    <ul className="flex flex-wrap gap-12 lg:gap-8 xl:gap-12">
       {links.map((link) => (
         <li key={link._uid}>
           <FooterLink {...link} />
