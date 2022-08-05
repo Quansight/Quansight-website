ad2220a9a9012b2436099861f4f18d8fdc2e5a5a
Brian Skinn <brian.skinn@gmail.com>
2022-08-05 13:45:07 -0400

    Further fix Script import?

--- apps/consulting/pages/_document.tsx
+++ apps/consulting/pages/_document.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 import { Html, Head, Main, NextScript } from 'next/document';
-import { Script } from 'next/script';
+import Script from 'next/script';
 
 const Document: FC = () => {
   return (
