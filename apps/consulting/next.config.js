36cbf666880364242a3953ee8306a23da2c41cfe
Brian Skinn <brian.skinn@gmail.com>
2022-08-12 14:42:31 -0400

    Add labs.quansight.com -> .org redirect (#413)
    Redirect all hosts starting with `labs` on to labs.quansight.org.
    
    This should:
    
    - Work cleanly for `labs.quansight.com`
    - Not interfere with any normal function of the LLC site
    - Also allow testing of the Vercel preview domains
      - The Vercel preview domains have to start with `labs-`,
        so the regex checks for either `labs.` or `labs-`
    
    The redirects were rearranged to put the
    labs.quansight.com entry first. Otherwise the redirects
    that are internal to qs.com can mess
    up pass-through paths to labs.quansight.org.
    Right now `/blog` is the
    main one, but there might be others...

--- apps/consulting/next.config.js
+++ apps/consulting/next.config.js
@@ -8,6 +8,55 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: '/:path*',
+        has: [
+          {
+            /* The purpose of this redirect is to take all traffic entering at
+               https://labs.quansight.com/:path and redirect it to
+               https://labs.quansight.org/:path. The value here is a regex
+               matching the host of an incoming request, which will find
+               the initial `labs.` of https://labs.quansight.com.
+            
+               The reason why a literal `labs.quansight.com` was not used
+               was to allow testing of the redirect before deployment.
+               During work on the PR, a temporary Vercel deployment was
+               created with a `labs-` prefix on the domain, to trigger
+               this redirect. (Vercel does not allow multiple layers of
+               subdomains on deployment URLs, so a `labs.` prefix
+               was not an option.) This is why the regex includes the `[.-]`
+               character class, instead of just `\\.` to match a period.
+               
+               While a tighter scope on this host regex match would probably
+               be ideal, since we are not planning on using anything other
+               than the apex quansight.com domain for the website
+               this configuration should not cause problems.
+               
+               Note that this redirect configuration requires that *BOTH*
+               `quansight.com` *and* `labs.quansight.com` be configured
+               as active domains for the `main` branch under the Vercel
+               project responsible for live deployment of the website.
+               Otherwise, incoming traffic to `labs.quansight.com/:path`
+               will receive a Vercel "no deployment at this address"
+               404 error.
+
+               This redirect is placed first in the sequence so that
+               all incoming traffic to a labs.quansight.com/:path is
+               forwarded to labs.quansight.org/:path without being
+               modified by the other redirects below.
+             */
+            type: 'host',
+            value: '^labs[.-].+',
+          },
+        ],
+        permanent: true,
+        destination: 'https://labs.quansight.org/:path*',
+      },
+      {
+        source: '/labs',
+        permanent: true,
+        destination: 'https://labs.quansight.org',
+      },
+      {
         source: '/library',
         has: [
           {
@@ -20,19 +69,14 @@ const nextConfig = {
         destination: '/library?page=1',
       },
       {
-        source: '/blog',
-        permanent: true,
-        destination: '/library?page=1&type=blog',
-      },
-      {
         source: '/staffing',
         permanent: true,
         destination: '/careers',
       },
       {
-        source: '/labs',
+        source: '/blog',
         permanent: true,
-        destination: 'https://labs.quansight.org',
+        destination: '/library?page=1&type=blog',
       },
     ];
   },
