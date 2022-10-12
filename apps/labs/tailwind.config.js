93ffccb81c5c991489414ee871eb33f91e2e97be
kherma <kamil.herma01@gmail.com>
2022-10-12 17:45:39 +0200

    feat(labs):[QUAN-15]: add font family for inline code

--- apps/labs/tailwind.config.js
+++ apps/labs/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       fontFamily: {
         heading: ['Mukta', 'sans-serif'],
         sans: ['Inter', 'sans-serif'],
+        code: ['Fira Code', 'monospace'],
       },
       colors: {
         transparent: 'transparent',
