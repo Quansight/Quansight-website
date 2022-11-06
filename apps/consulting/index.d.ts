9a2822c16096c36ea975b932ade9fe4f09e3b643
kherma <kamil.herma01@gmail.com>
2022-11-06 21:45:14 +0100

    feat(llc/labs):[QUAN-12]: extend Window interface with gtag property

--- apps/consulting/index.d.ts
+++ apps/consulting/index.d.ts
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.svg' {
-  const content: any
-  export const ReactComponent: any
-  export default content
+  const content: any;
+  export const ReactComponent: any;
+  export default content;
+
+  declare global {
+    interface Window {
+      gtag: any;
+    }
+  }
 }
--- apps/labs/index.d.ts
+++ apps/labs/index.d.ts
@@ -3,4 +3,10 @@ declare module '*.svg' {
   const content: any;
   export const ReactComponent: any;
   export default content;
+
+  declare global {
+    interface Window {
+      gtag: any;
+    }
+  }
 }
