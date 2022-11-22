cb129b97da6e0993dd524bd3cc9102e4b4840dc7
gabalafou <gabriel@fouasnon.com>
2022-11-22 16:30:46 -0500

    Revert "feat/quan 12 implement google ads on llc (#554)"
    This reverts commit 0c7ffbac8d97a10cdc664ceddf35a23ab7a5002c.

--- apps/consulting/pages/_app.tsx
+++ apps/consulting/pages/_app.tsx
@@ -12,18 +12,6 @@ function CustomApp({ Component, pageProps }: AppProps): JSX.Element {
       <Meta />
       <Component {...pageProps} />
 
-      {/* These two scripts are included verbatim as per gtag instructions */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script id="google-analytics-script" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
-      </Script>
-
       {typeof window !== 'undefined' &&
         window.location.hostname === consultingDomain && (
           // For more info about this script, see note in consulting/next.config.js
