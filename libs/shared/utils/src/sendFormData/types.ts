a9bb300950855a95d476d7215c0f3fdbe8c7c8ee
Brian Skinn <brian.skinn@gmail.com>
2022-12-19 14:54:33 -0500

    [FEAT]: Add pass-through of UTM parameters to Zapier (#613)
    * Store gclid & utm params to sessionStorage
    
    * Add GCLID & UTM tags to Zapier payload
    
    * Make sessionStorage names more unique
    
    On the *off-chance* that there's another site that's stuffing
    UTM or gclid data into sessionStorage, better to make the
    member names less collision-prone.
    
    * Add some explanatory comments
    
    * Disable GCLID capture
    
    * Streamline param extraction from window.location
    
    From code review
    
    Co-authored-by: gabalafou <gabriel@fouasnon.com>
    
    * Fix multiple spots per code review
    
    * Remove dead object-combining function
    
    * Remove commented-out combiner code
    
    * Switch missing UTMs to empty string
    
    Instead of undefined.
    
    This way, data for all five UTM tags is *always* passed to Zapier,
    hopefully minimizing any flakiness to the Zap behavior.
    
    * Add note about afterInteractive strategy
    
    Co-authored-by: gabalafou <gabriel@fouasnon.com>

--- libs/shared/utils/src/sendFormData/types.ts
+++ libs/shared/utils/src/sendFormData/types.ts
@@ -9,3 +9,27 @@ export type FormValues = {
   company: string;
   message: string;
 };
+
+// GCLID capture disabled for now
+export type TrackingParams = {
+  // gclid: string;
+  utm_source: string;
+  utm_medium: string;
+  utm_campaign: string;
+  utm_content: string;
+  utm_term: string;
+};
+
+export type FormAndTrackingValues = {
+  name: string;
+  email: string;
+  phone: string;
+  company: string;
+  message: string;
+  // gclid: string;
+  utm_source: string;
+  utm_medium: string;
+  utm_campaign: string;
+  utm_content: string;
+  utm_term: string;
+};
