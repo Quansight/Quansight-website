55fa95574366ce56887780d8e797c26948d6d405
Brian Skinn <brian.skinn@gmail.com>
2022-12-21 16:51:05 -0500

    Remove comment explaining disabled type filter

--- apps/consulting/components/Filters/Filters.tsx
+++ apps/consulting/components/Filters/Filters.tsx
@@ -43,9 +43,6 @@ export const Filters: FC<TFiltersProps> = ({
 
   return (
     <div className="flex flex-col gap-[2rem] justify-between items-center pb-[5.2rem] w-full sm:flex-row sm:gap-[5rem]">
-      {/*
-      We're removing the type filter until we have content of other types. Right now, we just have blog-type content.
-  */}
       <FilterMenu
         filterMenuVariant={FilterMenuVariant.Type}
         menuDataCurrent={postFilters.type}
