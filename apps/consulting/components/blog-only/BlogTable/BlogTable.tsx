18e36ed8093c8b04b658115ee157aa9adb318071
Brian Skinn <brian.skinn@gmail.com>
2023-01-17 11:16:41 -0500

    Complete BlogTable implementation

--- apps/consulting/components/blog-only/BlogTable/BlogTable.tsx
+++ apps/consulting/components/blog-only/BlogTable/BlogTable.tsx
@@ -5,14 +5,41 @@ export type TBlogTableProps = {
   headings: Array<string>;
   data: Array<Array<string>>;
   headingCellClass: string;
-};
-
-export type TBlogTableRowProps = {
-  rowData: Array<string>;
+  regularCellClass: string;
 };
 
 const makeHeadingCell = (cellData: string, cellClass: string) => {
   return <th className={cellClass}>{cellData}</th>;
+};
+
+const makeHeadingCellPartial = (cellClass: string) => {
+  return (cellData: string) => {
+    return makeHeadingCell(cellData, cellClass);
+  };
+};
+
+const makeHeadingRow = (cellData: Array<string>, cellClass: string) => {
+  return <tr>{cellData.map(makeHeadingCellPartial(cellClass))}</tr>;
+};
+
+const makeRegularCell = (cellData: string, cellClass: string) => {
+  return <td className={cellClass}>{cellData}</td>;
+};
+
+const makeRegularCellPartial = (cellClass: string) => {
+  return (cellData: string) => {
+    return makeRegularCell(cellData, cellClass);
+  };
+};
+
+const makeRegularRow = (cellData: Array<string>, cellClass: string) => {
+  return <tr>{cellData.map(makeRegularCellPartial(cellClass))}</tr>;
+};
+
+const makeRegularRowPartial = (cellClass: string) => {
+  return (cellData: Array<string>) => {
+    return makeRegularRow(cellData, cellClass);
+  };
 };
 
 export const BlogTable: FC<TBlogTableProps> = ({
@@ -23,14 +50,19 @@ export const BlogTable: FC<TBlogTableProps> = ({
     ['Cell 2,1', 'Cell 2,2'],
   ],
   headingCellClass = 'pl-8 pr-12 w-64',
+  regularCellClass = 'px-12',
 }) => {
   return (
     <div className="flex pl-16 md:justify-center md:pl-0 justify-left">
       <div className={`w-${tableWidth}`}>
         <table className="table-fixed">
-          <thead>
-            <tr></tr>
-          </thead>
+          <thead>{makeHeadingRow(headings, headingCellClass)}</thead>
+          <tbody>
+            {data.map(makeRegularRowPartial(regularCellClass))}
+            <tr className="h-0">
+              {/* This adds the bottom border line to the table */}
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
