7dbd01cb5fb109d8ddc199a505421de81bc1057a
gabalafou <gabriel@fouasnon.com>
2023-02-10 18:21:27 -0300

    add library and blog to restricted slugs

--- libs/shared/utils/src/getPaths/restrictedSlugs.ts
+++ libs/shared/utils/src/getPaths/restrictedSlugs.ts
@@ -1,8 +1,13 @@
+// These are all of the Storyblok slugs that should not match pages/[slug]
+// because they are matched to other pages. For example, Labs 'blog' is matched
+// to apps/labs/pages/blog/index.tsx.
 export const restrictedSlugs: string[] = [
   // Consulting
   'homepage',
   'about-us',
+  'library',
   // Labs
   'home',
   'team',
+  'blog',
 ];
